7bf3b9ba2bf212f48fa15e7a108b0d611026dd4b
James Tacker <james.tacker@saucelabs.com>
2020-09-02 09:31:16 -0700

    updated footer logo position

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -17,25 +17,25 @@ function FooterLink({to, href, label, prependBaseUrlToHref, ...props}) {
     forcePrependBaseUrl: true,
   });
   return (
-    <Link
-      className="footer__link-item"
-      {...(href
-        ? {
-            target: '_blank',
-            rel: 'noopener noreferrer',
-            href: prependBaseUrlToHref ? normalizedHref : href,
-          }
-        : {
-            to: toUrl,
-          })}
-      {...props}>
-      {label}
-    </Link>
+      <Link
+          className="footer__link-item"
+          {...(href
+              ? {
+                target: '_blank',
+                rel: 'noopener noreferrer',
+                href: prependBaseUrlToHref ? normalizedHref : href,
+              }
+              : {
+                to: toUrl,
+              })}
+          {...props}>
+        {label}
+      </Link>
   );
 }
 
 const FooterLogo = ({url, alt}) => (
-  <img className="footer__logo" alt={alt} src={url} />
+    <img className="footer__logo" alt={alt} src={url} />
 );
 
 function Footer() {
@@ -51,72 +51,69 @@ function Footer() {
   }
 
   return (
-    <footer
-      className={clsx('footer', {
-        'footer--dark': footer.style === 'dark',
-      })}>
-      <div className="container">
-        {links && links.length > 0 && (
-          <div className="row footer__links">
-            {links.map((linkItem, i) => (
-              <div key={i} className="col footer__col">
-                {linkItem.title != null ? (
-                  <h4 className="footer__title">{linkItem.title}</h4>
-                ) : null}
-                {linkItem.items != null &&
-                Array.isArray(linkItem.items) &&
-                linkItem.items.length > 0 ? (
-                  <ul className="footer__items">
-                    {linkItem.items.map((item, key) =>
-                      item.html ? (
-                        <li
-                          key={key}
-                          className="footer__item" // Developer provided the HTML, so assume it's safe.
-                          // eslint-disable-next-line react/no-danger
-                          dangerouslySetInnerHTML={{
-                            __html: item.html,
-                          }}
-                        />
-                      ) : (
-                        <li key={item.href || item.to} className="footer__item">
-                          <FooterLink {...item} />
-                        </li>
-                      ),
-                    )}
-                  </ul>
-                ) : null}
+      <footer
+          className={clsx('footer', {
+            'footer--dark': footer.style === 'dark',
+          })}>
+        <div className="container">
+          <a
+              href={logo.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.footerLogoLink}>
+            <FooterLogo alt={logo.alt} url={logoUrl} />
+          </a>
+          {links && links.length > 0 && (
+              <div className="row footer__links">
+                {links.map((linkItem, i) => (
+                    <div key={i} className="col footer__col">
+                      {linkItem.title != null ? (
+                          <h4 className="footer__title">{linkItem.title}</h4>
+                      ) : null}
+                      {linkItem.items != null &&
+                      Array.isArray(linkItem.items) &&
+                      linkItem.items.length > 0 ? (
+                          <ul className="footer__items">
+                            {linkItem.items.map((item, key) =>
+                                item.html ? (
+                                    <li
+                                        key={key}
+                                        className="footer__item" // Developer provided the HTML, so assume it's safe.
+                                        // eslint-disable-next-line react/no-danger
+                                        dangerouslySetInnerHTML={{
+                                          __html: item.html,
+                                        }}
+                                    />
+                                ) : (
+                                    <li key={item.href || item.to} className="footer__item">
+                                      <FooterLink {...item} />
+                                    </li>
+                                ),
+                            )}
+                          </ul>
+                      ) : null}
+                    </div>
+                ))}
               </div>
-            ))}
-          </div>
-        )}
-        {(logo || copyright) && (
-          <div className="text--center">
-            {logo && logo.src && (
-              <div className="margin-bottom--sm">
-                {logo.href ? (
-                  <a
-                    href={logo.href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={styles.footerLogoLink}>
-                    <FooterLogo alt={logo.alt} url={logoUrl} />
-                  </a>
-                ) : (
-                  <FooterLogo alt={logo.alt} url={logoUrl} />
-                )}
-              </div>
-            )}
+          )}
+          {(logo || copyright) && (
+              <div className="text--center">
+                {logo && logo.src && (
+                    <div className="margin-bottom--sm">
 
-            <div // Developer provided the HTML, so assume it's safe.
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{
-                __html: copyright,
-              }}
-            />
-          </div>
-        )}
-      </div>
-    </footer>
+                    </div>
+                )}
+
+                <div // Developer provided the HTML, so assume it's safe.
+                    // eslint-disable-next-line react/no-danger
+                    dangerouslySetInnerHTML={{
+                      __html: copyright,
+                    }}
+                />
+              </div>
+          )}
+        </div>
+      </footer>
   );
 }
 
