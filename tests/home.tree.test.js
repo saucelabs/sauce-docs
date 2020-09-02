8a9169c10f9995d77dc632c212d33f1ba2b4811e
James Tacker <james.tacker@saucelabs.com>
2020-09-02 11:31:54 -0700

    updated cypress tests

--- tests/home.tree.test.js
+++ tests/home.tree.test.js
@@ -4,22 +4,11 @@ context('Home Page Tree Actions', () => {
         cy.visit('http://localhost:3000/')
     });
 
-    it('Why Sauce Labs Page', () => {
-        cy.contains('Why Sauce Labs?')
+    it('Landing Page', () => {
+        cy.contains('Welcome')
             .click();
         cy.url()
             .should('include', '/');
-        cy.get('[href="#cross-browser-cross-device-testing"]')
-            .should('contain', '#')
-            .click({multiple:true});
-        cy.url()
-            .should('include', '/#cross-browser-cross-device-testing');
-
-        cy.get('[href="#debugging-and-analytic-tools"]')
-            .should('contain', '#')
-            .click({multiple:true});
-        cy.url()
-            .should('include', '/#debugging-and-analytic-tools');
     });
 
     it('Getting Started Page', () => {
@@ -34,16 +23,5 @@ context('Home Page Tree Actions', () => {
             .click();
         cy.url()
             .should('include', '/saucelabs-application-storage');
-        cy.get('.token.assign-left.variable')
-            .should('contain','SAUCE_USERNAME' && 'SAUCE_ACCESS_KEY');
-        cy.get('.token.operator')
-            .should('contain', '=');
-        cy.get('.token.string')
-            .should('contain', 'valid.username' && 'valid.key');
-        cy.get('[href="#updating-webdriver-capabilities"]')
-            .should('contain', '#')
-            .click({multiple:true});
-        cy.url()
-            .should('include', '/saucelabs-application-storage#updating-webdriver-capabilities');
     })
 });
