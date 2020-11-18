48fcc8fc1ebc414f5675489bfb96b6c3c5b9f143
spider-sauce <james.tacker@saucelabs.com>
2020-11-18 15:24:25 -0800

    added web typekit and prism syntax

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,13 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'saucelabs', // Usually your GitHub org/user name.
   projectName: 'sauce-docs', // Usually your repo name.
+  stylesheets: [
+    'https://use.typekit.net/zmt8tam.css'
+  ],
   themeConfig: {
+    prism: {
+      additionalLanguages: ['java', 'ruby', 'csharp', 'bash', 'powershell', 'python'],
+    },
     algolia: {
       appId: 'ZETDNHTKFC',
       apiKey: '8442c4c56cae89e0f1a1b7c9a8fd8f9c',
