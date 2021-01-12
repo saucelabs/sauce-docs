dbc213b0e6370524e7f8a32fcff9e9d252d17a84
spider-sauce <james.tacker@saucelabs.com>
2021-01-11 17:48:19 -0800

    updated espresso-xcuitest broken link

---
+++ docs/mobile-apps/automated-testing/espresso-xcuitest.md
@@ -0,0 +1,11 @@
+---
+id: espresso-xcuitest
+title: Automated Testing with Espresso and XCUITest
+sidebar_label: Getting Started
+description: Landing Page for Espresso and XCUITest
+hide_table_of_contents: true
+---
+Please select the relevant use case for mobile application testing:
+
+* [Real Device Testing](/mobile-apps/automated-testing/espresso-xcuitest/real-device-testing)
+* [Virtual Device Testing](/mobile-apps/automated-testing/espresso-xcuitest/virtual-device-testing)
--- sidebars.js
+++ sidebars.js
@@ -102,6 +102,7 @@ module.exports = {
                     label: 'Espresso and XCUITest',
                     collapsed: true,
                     items: [
+                        'mobile-apps/automated-testing/espresso-xcuitest',
                         'mobile-apps/automated-testing/espresso-xcuitest/real-device-testing',
                         'mobile-apps/automated-testing/espresso-xcuitest/virtual-device-testing',
                         'mobile-apps/automated-testing/espresso-xcuitest/example-configurations',
