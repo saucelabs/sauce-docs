a6a3e243129744b201a8aeb5a66cb194c885b9fc
Alex Plischke <alex.plischke@saucelabs.com>
2021-03-04 14:53:18 -0800

    Wait for the tunnel to be ready

---
+++ awaitfile.sh
@@ -0,0 +1,15 @@
+deathclock=3
+
+printf "Waiting for file $1 "
+while [ ! -f $1 ];
+do
+  if [ "$deathclock" == 0 ]; then
+    echo " ERROR: Timeout while waiting for file $1"
+    exit 1
+  fi
+
+  sleep 1
+  printf '.' > /dev/tty
+
+  ((deathclock--))
+done
