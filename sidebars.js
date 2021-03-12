eda02659c9103433e0897587c6fdb382f4b17e2a
Laura Thomas <laura.thomas@saucelabs.com>
2021-03-12 09:46:08 -0600

    Fixed nav structure

--- sidebars.js
+++ sidebars.js
@@ -88,37 +88,41 @@ module.exports = {
         },
       'secure-connections/ipsec-vpn',
       ],
+
+
   "Mobile Apps": [
     'mobile-apps',
-     {
-        type: 'category',
-        label: 'Live Testing',
-        collapsed: true,
-        items: [
-          'mobile-apps/live-testing',
-              {
-                  type: 'category',
-                  label: 'Mobile Real Device Testing',
-                  collapsed: true,
-                  items: [
-                     'mobile-apps/live-testing/mobile-real-device/setting-up-a-test',
-                     'mobile-apps/live-testing/mobile-real-device/live-native-mobile-real',
-                     'mobile-apps/live-testing/mobile-real-device/testing-apple-pay',
-                 ],
-              },
-              {
-                  type: 'category',
-                  label: 'Mobile Virtual Device Testing',
-                  collapsed: true,
-                  items: [
-                     'mobile-apps/live-testing/mobile-virtual-device/setting-up-a-test',
-                     'mobile-apps/live-testing/mobile-virtual-device/live-native-mobile-virtual',
-                 ],
-              },
-              'mobile-apps/live-testing/device-vitals-mobile',
-              'mobile-apps/live-testing/camera-image-injection',
-              'mobile-apps/live-testing/app-storage',
-            ]
+    {
+         type: 'category',
+         label: 'Live Testing',
+         collapsed: true,
+         items: [
+           'mobile-apps/live-testing',
+
+               {
+                 type: 'category',
+                 label: 'Mobile Real Device Testing',
+                 collapsed: true,
+                 items: [
+                    'mobile-apps/live-testing/mobile-real-device/setting-up-a-test',
+                    'mobile-apps/live-testing/mobile-real-device/live-native-mobile-real',
+                    'mobile-apps/live-testing/mobile-real-device/testing-apple-pay',
+                   ],
+               },
+               {
+                 type: 'category',
+                 label: 'Mobile Virtual Device Testing',
+                 collapsed: true,
+                 items: [
+                    'mobile-apps/live-testing/mobile-virtual-device/setting-up-a-test',
+                    'mobile-apps/live-testing/mobile-virtual-device/live-native-mobile-virtual',
+                   ],
+               },
+               'mobile-apps/live-testing/device-vitals-mobile',
+               'mobile-apps/live-testing/camera-image-injection',
+             ],
+          },
+
         {
           type: 'category',
           label: 'Automated Testing',
@@ -150,11 +154,8 @@ module.exports = {
                 'mobile-apps/automated-testing/troubleshooting',
               ],
            },
-     'mobile-apps/app-storage',
-     'mobile-apps/features',
-     'mobile-apps/virtual-usb',
-     'mobile-apps/faq',
-         ],
+],
+
     "Web Apps": [
       'web-apps',
       {
@@ -163,6 +164,9 @@ module.exports = {
         collapsed: true,
         items: [
           'web-apps/live-testing',
+          'web-apps/live-testing/setting-up-a-test',
+          'web-apps/live-testing/live-testing-desktop',
+          'web-apps/live-testing/live-testing-mobile',
         ],
       },
       {
