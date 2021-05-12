5014779d4abfb205158902d39691779557c64f3f
James Tacker <45496368+spider-sauce@users.noreply.github.com>
2021-05-12 08:18:08 -0700

    updated index links (#371)

--- src/pages/home-callouts.js
+++ src/pages/home-callouts.js
@@ -14,13 +14,13 @@ class HomeCallouts extends Component {
           <p>See how to open a secure "tunnel" so that you can test a website or app hosted on your local computer or behind a corporate ﬁrewall.</p>
         </div>
         <div className="callout">
-          <h2><a href="/web-apps/automated-testing">Quick Start with Your Tests</a></h2>
+          <h2><a href="https://wiki.saucelabs.com/display/DOCS/Automated+Web+App+Testing+on+Desktop+and+Mobile+Browsers">Quick Start with Your Tests</a></h2>
           <p>If you already have automated tests, you're only a few steps away from running them on the world's largest continuous testing cloud.</p>
         </div>
       </div>
       <div className="callouts__container">
         <div className="callout">
-          <h2><a href="/mobile-apps/live-testing">Live Testing</a></h2>
+          <h2><a href="/mobile-apps/live-testing/live-mobile-app-testing">Live Testing</a></h2>
           <p>See how your app or site performs on any supported browser, OS, or device, when you manually test it in our UI.</p>
         </div>
         <div className="callout">
