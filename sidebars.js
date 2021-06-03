b9c7f06b1ea02ce8fa4ab6f892a8212eac97f2d6
Nancy Sweeney <nancy.sweeney@saucelabs.com>
2021-06-03 13:25:52 -0400

    New Jira Integration doc

--- sidebars.js
+++ sidebars.js
@@ -70,6 +70,7 @@ module.exports = {
                         collapsed: true,
                         items: [
                             'basics/integrations/deque',
+                            'basics/integrations/jira',
                         ],
                     },
                 ],
