5f02d15c8fdfc0f539ff0e5087618314398badf3
James Tacker <james.tacker@saucelabs.com>
2021-06-16 12:53:44 -0700

    updated getData

--- src/components/getData.js
+++ src/components/getData.js
@@ -3,8 +3,9 @@ const url = `https://api.us-west-1.saucelabs.com/rest/v1/public/tunnels/info/ver
 const getData = async () => {
     return await axios.get(url
     ).then(response => {
+        let i = 0;
         const results = Object.entries(response.data.downloads)
-            .map(([key, val])=>({platform: key, ...val}));
+            .map(([key, val])=>({platform: key, ...val, id: i++, ...val}));
         console.log(results);
         return results;
     }).catch(err => console.log(err));
--- src/components/scTable.jsx
+++ src/components/scTable.jsx
@@ -1,27 +1,26 @@
 import React, {useEffect} from "react";
-import Table from "./Table";
+const axios = require("axios");
 
-const ScTable = () => {
-    const theadData = ["Platform", "Download Link", "SHA1 Checksum"];
-    // const tbodyData = [];
-    const tbodyData = [
-        {
-            id: "1",
-            items: ["linux", "https://saucelabs.com/downloads/sc-4.6.5-linux.tar.gz", "caa05e8779f4e052463a63e1b38f6362473ce644"]
-        },
-        {
-            id: "2",
-            items: ["osx", "https://saucelabs.com/downloads/sc-4.6.5-osx.zip", "caa05e8779f4e052463a63e1b38f6362473ce644"]
-        },
-        {
-            id: "3",
-            items: ["win32", "https://saucelabs.com/downloads/sc-4.6.5-win32.zip", "4431030b57dc0c416ea59a2afa6738b0de59dc80"]
-        },
-    ];
-    return(
-        <div>
-            <Table theadData={theadData} tbodyData={tbodyData} />
-        </div>
-    );
+const ScTable = async () => {
+    return await axios.get(url
+    ).then(response => {
+        let i = 0;
+        const results = Object.entries(response.data.downloads)
+            .map(([key, val])=>({platform: key, ...val, id: i++, ...val}));
+        console.log(results);
+        return(
+            <div>
+                <table>
+                    {getData().map(el => (
+                        <tr>
+                            <td>{el.platform}</td>
+                            <td>{el.download_url}</td>
+                            <td>{el.sha1}</td>
+                        </tr>
+                    ))}
+                </table>
+            </div>
+        );
+    }).catch(err => console.log(err));
 };
 export default ScTable;
