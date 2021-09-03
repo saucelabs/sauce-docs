2d21980d9928eab9e26bb92e432ce567a5e300b0
James Tacker <james.tacker@saucelabs.com>
2021-09-03 10:52:35 -0700

    fixed test destination

--- tests/cypress/integration/home.tree.test.js
+++ tests/cypress/integration/home.tree.test.js
@@ -2,7 +2,7 @@
 //TODO Harden Cypress Locators
 context('Home Page Tree Actions', () => {
     beforeEach(() => {
-        cy.visit(`http://localhost:3000/overview`)
+        cy.visit(`/overview`)
         cy.viewport(1536, 960)
     });
 
