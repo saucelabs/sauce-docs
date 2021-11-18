008cc450529455419a632d52d152a80ee3d75085
discombobulateme <paloma.oliveira@saucelabs.com>
2021-11-18 22:59:01 +0100

    [COM-18]Add quiskstart component

--- src/pages/quickstart.js
+++ src/pages/quickstart.js
@@ -5,27 +5,51 @@ class QuickStart extends Component {
     return (
       <div>
         <h2 className="title">QuickStart</h2>
-        <div className="callouts__container">
-          <div className="callout">
-            <h2>Selenium</h2>
-            <p>Learn how to use Selenium like a pro.</p>
-            <h4><a href="">&gt;&gt;Take a course</a></h4>
-            <h4><a href="">&gt;&gt;Go to sample code</a></h4>
+        <div className="quickstart__container">
+          <div className="quickstart">
+            <img src='img/quickstart/appium.svg'  alt="Appium Logo"/>
+            <h3>
+              <a href="https://docs.saucelabs.com/mobile-apps/automated-testing/appium/quickstart/"
+                target="_blank" rel="noopener noreferrer">
+                Appium
+              </a>
+            </h3>
           </div>
-          <div className="callout">
-            <h2>Stack Overflow</h2>
-            <p>Tag saucelabs to get your questions answered by our experts.</p>
-            <h4><a href="">&gt;&gt;Join the conversation</a></h4>
+          <div className="quickstart">
+            <img src='img/quickstart/cypress.svg'  alt="Cypress Logo"/>
+            <h3>
+              <a href="https://docs.saucelabs.com/web-apps/automated-testing/cypress/quickstart/"
+                target="_blank" rel="noopener noreferrer">
+                Cypress
+              </a>
+            </h3>
           </div>
-          <div className="callout">
-            <h2>Engineering Blog</h2>
-            <p>Get expert advice from our engineering and open source teams.</p>
-            <h4><a href="">&gt;&gt;Read the tips</a></h4>
+          <div className="quickstart">
+            <img src='img/quickstart/playwright.svg'  alt="PlayWright Logo"/>
+            <h3>
+              <a href="https://docs.saucelabs.com/web-apps/automated-testing/playwright/quickstart/"
+                target="_blank" rel="noopener noreferrer">
+                PlayWright
+              </a>
+            </h3>
           </div>
-          <div className="callout">
-            <h2>Sample Scripts</h2>
-            <p>Get up and running quickly using Sauce Labs training code samples.</p>
-            <h4><a href="">&gt;&gt;Go to GitHub</a></h4>
+          <div className="quickstart">
+            <img src='img/quickstart/puppeteer.svg'  alt="Puppeteer Logo"/>
+            <h3>
+              <a href="https://docs.saucelabs.com/web-apps/automated-testing/puppeteer/quickstart/"
+                target="_blank" rel="noopener noreferrer">
+                Puppeteer
+              </a>
+            </h3>
+          </div>
+          <div className="quickstart">
+            <img src='img/quickstart/testcafe.svg' alt="TestCafe Logo"/>
+            <h3>
+              <a href="https://docs.saucelabs.com/web-apps/automated-testing/testcafe/quickstart/"
+                target="_blank" rel="noopener noreferrer">
+                TestCafe
+            </a>
+          </h3>
           </div>
         </div>
       </div>
