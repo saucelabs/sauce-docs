932edbf5be10098d0a9549cd39a6359d741db3bd
James Tacker <james.tacker@saucelabs.com>
2021-12-15 12:24:52 -0800

    fixed css

--- src/pages/home-search.js
+++ src/pages/home-search.js
@@ -1,10 +1,9 @@
 import React from "react";
 import SearchBar from "../theme/SearchBar";
-import styles from '/src/css/homesearchbar.css';
 
 const HomeSearch = () => (
     <div className={"new-search"}>
-        <SearchBar style={styles}/>
+        <SearchBar/>
     </div>
 );
 
