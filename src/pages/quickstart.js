7eb54554765dd8b8cbb1887fcfd4ac52fa80e9e6
discombobulateme <paloma.oliveira@saucelabs.com>
2022-02-21 12:48:58 +0100

    Delete . to relative path, fix carousel typo in component name

--- src/pages/quickstart.js
+++ src/pages/quickstart.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Caroulsel from '../components/caroulsel';
+import Carousel from '../components/carousel';
 
 class QuickStart extends Component {
   render() {
     return (
       <div>
         <h2 className="title-quickstart">Quickstart with Sauce Labs</h2>
-        <Caroulsel />
+        <Carousel />
       </div>
     );
   }
