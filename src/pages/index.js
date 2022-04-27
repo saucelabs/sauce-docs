a67c4c8d2a7a66c9b9dc5a4adbd50335bfe771f8
discombobulateme <paloma.oliveira@saucelabs.com>
2022-04-27 19:22:43 +0200

    Change hero area on landpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,33 +20,41 @@ function Home() {
     >
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className='container'>
-          <h1 className='hero__title'>{siteConfig.title}</h1>
-          <p className='hero__subtitle'>{siteConfig.tagline}</p>
-          <div className='new-search'>
-            <SearchBar />
+          <div>
+            <h1 className='hero__title'>{siteConfig.title}</h1>
+            <p className='hero__subtitle'>{siteConfig.tagline}</p>
+            <div className='new-search'>
+              <SearchBar />
+            </div>
+            {/* style needs to contain hero to override as hero has priority */}
+            <span className='hero__popular-searches'>
+              Popular Searches:&nbsp;
+              <a
+                className='hero__popular-searches'
+                href='https://docs.saucelabs.com/dev/cli/saucectl/#installing-saucectl/'
+              >
+                &nbsp;saucectl |
+              </a>
+              <a className='hero__popular-searches' href='https://docs.saucelabs.com/dev/cli/sauce-connect-proxy/'>
+                &nbsp;Sauce Connect |
+              </a>
+              <a className='hero__popular-searches' href='https://docs.saucelabs.com/api-testing/index.html'>
+                &nbsp;API Testing |
+              </a>
+              <a
+                className='hero__popular-searches'
+                href='https://docs.saucelabs.com/web-apps/automated-testing/selenium/selenium4/'
+              >
+                &nbsp;Selenium 4 |
+              </a>
+              <a className='hero__popular-searches' href='https://docs.saucelabs.com/dev/api/'>
+                &nbsp;REST API
+              </a>
+            </span>
           </div>
-          {/* style needs to contain hero to override as hero has priority */}
-          <span className='hero__popular-searches'>
-            Popular Searches:&nbsp;
-            <a className='hero__popular-searches' href='https://docs.saucelabs.com/dev/cli/saucectl/#installing-saucectl/'>
-              &nbsp;saucectl |
-            </a>
-            <a className='hero__popular-searches' href='https://docs.saucelabs.com/dev/cli/sauce-connect-proxy/'>
-              &nbsp;Sauce Connect |
-            </a>
-            <a className='hero__popular-searches' href='https://docs.saucelabs.com/api-testing/index.html'>
-              &nbsp;API Testing |
-            </a>
-            <a
-              className='hero__popular-searches'
-              href='https://docs.saucelabs.com/web-apps/automated-testing/selenium/selenium4/'
-            >
-              &nbsp;Selenium 4 |
-            </a>
-            <a className='hero__popular-searches' href='https://docs.saucelabs.com/dev/api/'>
-              &nbsp;REST API
-            </a>
-          </span>
+          <div>
+            <img className='landpage'  src='img/dev-resources-hero.png' />
+          </div>
         </div>
       </header>
       <main>
