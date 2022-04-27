5d392d2492871e607494ca5d2664b9cd504482ef
discombobulateme <paloma.oliveira@saucelabs.com>
2022-04-27 19:23:24 +0200

    Add div to separate quickstart in 2 columns

--- src/pages/quickstart.js
+++ src/pages/quickstart.js
@@ -5,8 +5,12 @@ class QuickStart extends Component {
   render() {
     return (
       <div>
-        <h2 className="title-quickstart">Quickstart with Sauce Labs</h2>
-        <Carousel />
+        <div>
+          <h2 className="title-quickstart">Quickstart with Sauce Labs</h2>
+        </div>
+        <div>
+          <Carousel />
+        </div>
       </div>
     );
   }
