597ea80d90c076a5533f506d691ccb38d3b9076e
discombobulateme <paloma.oliveira@saucelabs.com>
2022-05-06 17:14:13 +0200

    Chance component for new design 2 col

--- src/pages/quickstart.js
+++ src/pages/quickstart.js
@@ -4,12 +4,15 @@ import Carousel from '../components/carousel';
 class QuickStart extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <h2 className="title-quickstart">Quickstart with Sauce Labs</h2>
-        </div>
-        <div>
+      <div className='container-landpage'>
+        <div className='column-left'>
           <Carousel />
+        </div>
+        <div className='column-right'>
+          <h2 className='title-quickstart'>Quickstart with your favorite test suite</h2>
+          <p>We provide the tools you need to get started quickly with Sauce Labs
+            using your favorite framework.
+          </p>
         </div>
       </div>
     );
