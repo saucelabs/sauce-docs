c20724f4eb45e221459e7129e10cf949cfcc05a2
discombobulateme <paloma.oliveira@saucelabs.com>
2023-02-02 12:06:36 +0100

    Adjust new images

--- src/pages/community.js
+++ src/pages/community.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Button from '../components/button';
+import Button from "../components/button";
 
 class Community extends Component {
     render() {
         return (
-            <div className='container-landpage'>
-                <div className='column-left'>
+            <div className="container-landpage">
+                <div className="column-left">
                     <img
-                        className='landpage-community'
-                        src='img/important-links-hero.png'
+                        className="landpage-community"
+                        src="img/api-quality-tier-1.png"
                     />
                 </div>
-                <div className='column-right'>
+                <div className="column-right">
                     <h2>Get community support</h2>
                     <p>Can't find what you need? We are here to help.</p>
-                    <span className='landpage-buttons-container'>
+                    <span className="landpage-buttons-container">
                         <a
-                            href='https://stackoverflow.com/tags/saucelabs'
-                            target='_blank'
-                            rel='noopener noreferrer'
+                            href="https://stackoverflow.com/tags/saucelabs"
+                            target="_blank"
+                            rel="noopener noreferrer"
                         >
                             <Button
-                                className='button-light'
-                                name='Stack Overflow'
+                                className="button-light"
+                                name="Stack Overflow"
                             />
                         </a>
                         <a
-                            href='https://support.saucelabs.com/hc/en-us'
-                            target='_blank'
-                            rel='noopener noreferrer'
+                            href="https://support.saucelabs.com/hc/en-us"
+                            target="_blank"
+                            rel="noopener noreferrer"
                         >
                             <Button
-                                className='button-light'
-                                name='Customer Support'
+                                className="button-light"
+                                name="Customer Support"
                             />
                         </a>
                     </span>
