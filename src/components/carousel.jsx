2fa893fc84f9350a579d5d5b6a560ae079238a74
discombobulateme <paloma.oliveira@saucelabs.com>
2023-02-03 13:41:33 +0100

    Chance frameworks carousel icons

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,62 +1,54 @@
-import React from 'react';
+import React from "react";
 /* package: https://www.npmjs.com/package/react-alice-carousel */
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 const handleDragStart = (e) => e.preventDefault();
 
 const items = [
-    <div className='quickstart' onDragStart={handleDragStart}>
+    // eslint-disable-next-line react/jsx-key
+    <div className="quickstart" onDragStart={handleDragStart}>
         <a
-            href='/mobile-apps/automated-testing/appium/quickstart/'
-            target='_self'
+            href="/mobile-apps/automated-testing/appium/quickstart/"
+            target="_self"
         >
-            <img src='img/quickstart/appium.svg' alt='Appium Logo' />
+            <img src="img/quickstart/appium_icon_green.svg" alt="Appium Logo" />
             <h3>Appium</h3>
         </a>
     </div>,
-    <div className='quickstart' onDragStart={handleDragStart}>
+    <div className="quickstart" onDragStart={handleDragStart}>
         <a
-            href='/web-apps/automated-testing/cypress/quickstart/'
-            target='_self'
+            href="/web-apps/automated-testing/cypress/quickstart/"
+            target="_self"
         >
-            <img src='img/quickstart/cypress.svg' alt='Cypress Logo' />
+            <img
+                src="img/quickstart/cypress_icon_green.svg"
+                alt="Cypress Logo"
+            />
             <h3>Cypress</h3>
         </a>
     </div>,
-    <div className='quickstart' onDragStart={handleDragStart}>
+    <div className="quickstart" onDragStart={handleDragStart}>
         <a
-            href='/mobile-apps/automated-testing/espresso-xcuitest/'
-            target='_self'
+            href="/web-apps/automated-testing/selenium/quickstart/"
+            target="_self"
         >
-            <img src='img/quickstart/espresso.png' alt='Espresso Logo' />
-            <h3>Espresso</h3>
-        </a>
-    </div>,
-    <div className='quickstart' onDragStart={handleDragStart}>
-        <a
-            href='/web-apps/automated-testing/playwright/quickstart/'
-            target='_self'
-        >
-            <img src='img/quickstart/playwright.png' alt='Playwright Logo' />
-            <h3>Playwright</h3>
-        </a>
-    </div>,
-    <div className='quickstart' onDragStart={handleDragStart}>
-        <a
-            href='/web-apps/automated-testing/selenium/quickstart/'
-            target='_self'
-        >
-            <img src='img/quickstart/selenium.svg' alt='Selenium Logo' />
+            <img
+                src="img/quickstart/selenium_icon_green.svg"
+                alt="Selenium Logo"
+            />
             <h3>Selenium</h3>
         </a>
     </div>,
-    <div className='quickstart' onDragStart={handleDragStart}>
+    <div className="quickstart" onDragStart={handleDragStart}>
         <a
-            href='/mobile-apps/automated-testing/espresso-xcuitest/'
-            target='_self'
+            href="/mobile-apps/automated-testing/espresso-xcuitest/"
+            target="_self"
         >
-            <img src='img/quickstart/xcuitest.png' alt='XCUITest Logo' />
+            <img
+                src="img/quickstart/xcuitest_icon_green.svg"
+                alt="XCUITest Logo"
+            />
             <h3>XCUITest</h3>
         </a>
     </div>,
@@ -71,7 +63,7 @@ const responsive = {
 const Carousel = () => {
     return (
         <AliceCarousel
-            className='alice-carousel__prev-btn'
+            className="alice-carousel__prev-btn"
             mouseTracking
             items={items}
             responsive={responsive}
