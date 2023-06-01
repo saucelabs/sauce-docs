7d2fa944bf467dc0a03e4fc33d93d2d5eb685f85
Lysanne Pinto <lysannepinto@gmail.com>
2023-06-01 13:53:08 -0400

    Update index.js

--- tests/sauce-docs-checker/index.js
+++ tests/sauce-docs-checker/index.js
@@ -170,8 +170,7 @@ function get_links(map,title) {
 }
 
 async function parse_wiki_map() {
-  let API_BASE_URL = `https://gist.githubusercontent.com/tackcity/`;
-  let url = API_BASE_URL+`833de80d2f240683adcc2c0f49e8e19f/raw/be45564ebdc7edf1c2448d4021845e754e9a3fd0/map.conf`;
+  let url = `https://github.com/saucelabs/sauce-docs/blob/main/tests/sauce-docs-checker/map.conf`;
   let res = await axios.get(url);
   let text = res.data;
   // console.log(text);
