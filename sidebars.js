756ec2df22c42aba35b0d04f9ee9bb157c3373bc
Loredana <loredana.spangaro@saucelabs.com>
2023-06-28 13:08:34 +0200

    TCD-2818

--- sidebars.js
+++ sidebars.js
@@ -1344,7 +1344,6 @@ module.exports = {
                 'test-results',
                 'test-results/viewing-test-results',
                 'test-results/sharing-test-results',
-                'test-results/archived-test-results',
                 'test-results/test-status',
                 'test-results/badges-browser-matrix',
             ],
