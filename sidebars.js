e3f762635987fe4592b7263930c85d0c1a4c37ed
Loredana <loredana.spangaro@saucelabs.com>
2023-07-12 12:25:42 +0200

    TCD-2905:ptrace

--- sidebars.js
+++ sidebars.js
@@ -615,6 +615,7 @@ module.exports = {
                             ],
                         },
                         'error-reporting/advanced/morgue',
+                        'error-reporting/advanced/ptrace',
                     ],
                 },
             ],
