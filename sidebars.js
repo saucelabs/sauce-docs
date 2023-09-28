147491f53082ab19cdba9dcef50e9104491d9c3f
Dan Slov <dan.slov@saucelabs.com>
2023-09-28 15:58:29 -0700

    Updating sc5 flags (#2462)
    * Adding sauce-connect-5 homebrew install
    
    * Updating sc-5 CLI docs

--- sidebars.js
+++ sidebars.js
@@ -712,6 +712,7 @@ module.exports = {
                     label: 'Sauce Connect Proxy 5 CLI (alpha)',
                     collapsed: true,
                     items: [
+                        'dev/cli/sauce-connect-5',
                         'dev/cli/sauce-connect-5/run',
                         'dev/cli/sauce-connect-5/completion',
                     ],
