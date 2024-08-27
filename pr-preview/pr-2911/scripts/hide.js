d20d5bbaedf6054a34aa0463ca83ce09762a4449
alexplischke <alexplischke@users.noreply.github.com>
2024-08-27 23:14:45 +0000

    Deploy preview for PR 2911 🛫

---
+++ pr-preview/pr-2911/scripts/hide.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    document.getElementById('forethought-chat').setAttribute('class', 'hide');
+};
+
+function scrollFunc() {
+    if (window.pageYOffset > 800) {
+        document
+            .getElementById('forethought-chat')
+            .setAttribute('class', 'show');
+        // } else {
+        //     document.getElementById('forethought-chat').setAttribute('class', 'hide')
+    } else if (window.pageYOffset < 250) {
+        document
+            .getElementById('forethought-chat')
+            .setAttribute('class', 'hide');
+    }
+}
+
+window.onscroll = scrollFunc;
