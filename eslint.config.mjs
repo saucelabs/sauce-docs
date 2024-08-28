9aa2a89b95170168a15d5e33c74e08ea630a1ce8
Alex Plischke <alex.plischke@saucelabs.com>
2024-08-28 14:21:20 -0700

    build: migrate eslint to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import react from 'eslint-plugin-react';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginPromise from 'eslint-plugin-promise';
+
+export default [
+    js.configs.recommended,
+    pluginPromise.configs['flat/recommended'],
+    prettierRecommended,
+    {
+        files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+        ...react.configs.flat.recommended,
+    },
+    {
+        ignores: ['.docusaurus/', '**/*tests/', '**/*webdriver/', '**/*build/'],
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+
+        rules: {
+            'react/prop-types': 'off',
+            'prettier/prettier': ['error'],
+        },
+    },
+];
