b8749405aceaa8b3d85d15817f2e3cdc65b1bc24
tianfeng92 <tianfeng92@users.noreply.github.com>
2024-08-28 21:33:34 +0000

    Deploy preview for PR 2915 🛫

---
+++ pr-preview/pr-2915/scripts/hide.js
@@ -0,0 +1,1 @@
+function scrollFunc(){window.pageYOffset>800?document.getElementById("forethought-chat").setAttribute("class","show"):window.pageYOffset<250&&document.getElementById("forethought-chat").setAttribute("class","hide")}window.onload=function(){document.getElementById("forethought-chat").setAttribute("class","hide")},window.onscroll=scrollFunc;
