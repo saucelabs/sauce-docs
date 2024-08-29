2ddc010ec38d17b973d5d7e843a13bd95b82a179
kristofmuhi <kristof.muhi+1@saucelabs.com>
2024-08-29 09:56:30 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1138,7 +1138,7 @@ module.exports = {
         },
         {
             type: 'category',
-            label: 'App Distribution(TestFairy)',
+            label: 'App Distribution (TestFairy)',
             collapsed: true,
             items: [
                 'testfairy/testfairy',
